2b594f4f595eb6b3664b0235d2f954a80ec1995f
Brandon Erb <brandon.m.erb@gmail.com>
2023-10-20 00:25:45 -0400

    Nebula rename

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { useAtomicApi } from '@atomicdesign/atomic-singularity';
 import { useAtomicLogging } from "@atomicdesign/atomic-sdk/logging";
 import { useAtomicVue } from '@atomicdesign/atomic-vue';
-// import { OriginGovernor } from '@atomicdesign/atomic-origin';
+// import { OriginNebula } from '@atomicdesign/atomic-origin';
 
 useAtomicApi()
   .use(useAtomicLogging())
